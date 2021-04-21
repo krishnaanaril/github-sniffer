f947448933bf230acf5851f53624b733144743c1
Krishna Mohan <krishnamohan.a.m@gmail.com>
2021-04-21 11:52:14 +0530

    chore: update prod configuration
    update prod configuration with api values

--- projects/github-sniffer-web/src/environments/environment.prod.ts
+++ projects/github-sniffer-web/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  credentials: {
+    client_id: 'b8afef5f955ae3488bf3',
+    client_secret: '1e694388cf6769bdeb49c88c13b184d769a940bb'
+  },
+  github_api: {
+    base_url: 'https://api.github.com',
+    authorize_url: 'https://github.com/login/oauth/authorize',
+    search_issues_url: 'https://api.github.com/search/issues'
+  }
 };
